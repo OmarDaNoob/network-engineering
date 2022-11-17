ac9c9b08d19969079e311f982afdb2f43afcc0ec
OmarDaNoob <omareyad0101@hotmail.com>
2022-11-17 10:02:12 -0500

    changes to comparison

--- comparison.py
+++ comparison.py
@@ -5,58 +5,32 @@ password = "cisco123!"
 # Create the SSH session
 session = pexpect.spawn('ssh ' + username + '@' + ip_address,encoding='utf-8', timeout=20)
 result = session.expect(['[password:', pexpect.TIMEOUT, pexpect.EOF])
-# Check for error, if exists then display error and exit
-if result !=0:
-    print('--- FAILURE! creating session for: ', ip_address)
-    exit()
+
 session.sendline(password)
-result=session.expect('#',pexpect.TIMEOUT,pexpect.EOF)
-# Check for error, if exists then display error and exit
-if result !=0:
-    print('--- FAILURE! creating session for: ', ip_address)
-    exit()
+result=session.expect('#')
+
 #change host name
 session.sendline('conf t')
-result=session.expect('\(config\)',pexpect.TIMEOUT,pexpect.EOF)
-# Check for error, if exists then display error and exit
-if result !=0:
-    print('--- FAILURE! creating session for: ', ip_address)
-    exit()
+result=session.expect('\(config\)')
 
 session.sendline('hostname R1')
-result=session.expect('R1',pexpect.TIMEOUT,pexpect.EOF)
-# Check for error, if exists then display error and exit
-if result !=0:
-    print('--- FAILURE! creating session for: ', ip_address)
-    exit()
+result=session.expect('R1')
+
 #exit config
 session.sendline('exit')
-result=session.expect('#',pexpect.TIMEOUT,pexpect.EOF)
-# Check for error, if exists then display error and exit
-if result !=0:
-    print('--- FAILURE! creating session for: ', ip_address)
-    exit()
+result=session.expect('#')
+
 #copy running conf and startup conf
 session.sendline('terminal length 0')
-result=session.expect('#',pexpect.TIMEOUT,pexpect.EOF)
-# Check for error, if exists then display error and exit
-if result !=0:
-    print('--- FAILURE! creating session for: ', ip_address)
-    exit()
+result=session.expect('#')
+
 session.sendline('show run')
 running_conf=session.before.splitlines()
-result=session.expect('#',pexpect.TIMEOUT,pexpect.EOF)
-# Check for error, if exists then display error and exit
-if result !=0:
-    print('--- FAILURE! creating session for: ', ip_address)
-    exit()
+result=session.expect('#')
+
 session.sendline('show start')
 start_conf=sesion.before.splitlines()
-result=session.expect('#',pexpect.TIMEOUT,pexpect.EOF)
-# Check for error, if exists then display error and exit
-if result !=0:
-    print('--- FAILURE! creating session for: ', ip_address)
-    exit()
+result=session.expect('#')
 #exit enable mode
 session.sendline('exit')
 #get a sucess message if works
